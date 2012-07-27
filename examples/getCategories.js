322f06d5140afcafa68080952527dae89894aeac
hakubo <bukaj.kelo+github@gmail.com>
2012-07-27 17:18:37 +0200

    Add info of next batch

---
+++ examples/getCategories.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: olek
+ * Date: 27.07.2012
+ * Time: 15:03
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ examples/getUsers.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: olek
+ * Date: 27.07.2012
+ * Time: 17:14
+ * To change this template use File | Settings | File Templates.
+ */
