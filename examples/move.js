183681785bc0a787682399ac59a5bd2c48bfbe86
marcin <poczta.maciejewski@gmail.com>
2012-07-27 15:29:03 +0200

    move method introduced (4th PozWikiaDay!)

---
+++ examples/move.js
@@ -0,0 +1,16 @@
+/**
+ * Example script moving page
+ *
+ * @see http://www.mediawiki.org/wiki/API:Move
+ */
+
+var bot = require('../lib/bot').bot;
+
+var client = new bot('config.js');
+
+client.logIn(function() {
+	// move the page
+	client.move('newnewmtestmarcin', 'newnewnewmtestmarcin', 'test summary', function(userData) {
+		console.log(JSON.stringify(userData, null, '\t'));
+	});
+});
