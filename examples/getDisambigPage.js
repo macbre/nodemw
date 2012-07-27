b44b8bb619b5e6abb5606655e6fa6a8a3a0d0b38
macbre <maciej.brencz@gmail.com>
2012-07-27 14:24:35 +0200

    Add example script

---
+++ examples/getDisambigPage.js
@@ -0,0 +1,46 @@
+/**
+ * Example script preparing ship disambig pages for translation
+ */
+
+var bot = require('../lib/bot').bot;
+
+var client = new bot({
+	server: 'en.wikipedia.org',
+ 	path: '/w',
+	debug: false
+});
+
+var titles = [
+	'Manta',
+	'Maple',
+	'Marathon'
+];
+
+var template = "== USS $1 ==\n" +
+"{{s|disambig}}\n" +
+"[[Okręt]]y [[United States Navy|US Navy]] o nazwie '''[[Lista akronimów przed nazwami okrętów|USS]] \"$1\"'''\n" +
+"\n*Pierwszy\n*Drugi\n" +
+"\n$2\n" +
+"[[:Kategoria:Nazwy okrętów amerykańskich|$1]]\n" +
+"[[:en:USS $1]]";
+
+titles.forEach(function(title) {
+
+	client.getArticle('USS ' + title, function(content) {
+		//console.log(content);
+
+		var items = content.match(/\*(.*)\n/g);
+
+		//console.log(items);
+
+		var newContent = template.
+			replace(/\$1/g, title).
+			replace(/\$2/g, items.join(""));
+
+		//console.log('====');
+		console.log(newContent);
+
+		console.log("\n\n\n\n");
+	});
+
+});
