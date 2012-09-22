de078ce6949ea20ee6e5195206cdafcb845970f9
macbre <maciej.brencz@gmail.com>
2012-09-23 00:29:32 +0200

    Work on files upload

---
+++ examples/upload.js
@@ -0,0 +1,21 @@
+/**
+ * Example script for file uploads
+ *
+ * @see http://www.mediawiki.org/wiki/API:Upload
+ */
+
+var bot = require('../lib/bot').bot,
+	client = new bot('config.js');
+
+var url = 'http://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png',
+	fileName = 'UploadTest.png',
+	summary = 'Testing upload...';
+
+client.logIn(function() {
+
+	console.log('Uploading ' + url + '...');
+
+	client.uploadByUrl(fileName, url, summary, function(res) {
+		console.log('Upload completed!');
+	});
+});
