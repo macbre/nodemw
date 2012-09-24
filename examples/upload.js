01303c557bdd2fabd8b57ce74a6c01459cae54d7
macbre <maciej.brencz@gmail.com>
2012-09-24 14:55:53 +0200

    The Ultimate Fix For File Upload

--- examples/upload.js
+++ examples/upload.js
@@ -9,12 +9,16 @@ var bot = require('../lib/bot').bot,
 
 var url = 'http://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png',
 	fileName = 'UploadTest.png',
-	summary = 'Testing upload...';
+	summary = 'Testing upload ąęź...';
 
 client.logIn(function() {
 	console.log('Uploading ' + url + '...');
 
 	client.uploadByUrl(fileName, url, summary, function(res) {
 		console.log('Upload completed!');
+
+		client.edit('File:' + fileName, 'File description goes here ąęź\n\n[[Category:Foo]]', 'Adding a file description', function(res) {
+			console.log('File description edited');
+		});
 	});
 });
