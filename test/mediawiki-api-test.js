77cb24b690d2303a36a7618fafb3a058bd2c1578
Maciej <maciej.brencz@gmail.com>
2013-04-18 23:29:05 +0200

    Add tests for API methods

--- test/mediawiki-api-test.js
+++ test/mediawiki-api-test.js
@@ -24,6 +24,34 @@ function callback() {
 }
 
 vows.describe('Mediawiki API').addBatch({
+	'client,api.call()': {
+		topic: function() {
+			// http://en.wikipedia.org/w/api.php?action=query&meta=siteinfo&siprop=namespaces&format=json
+			var params = {
+				action: 'query',
+				meta: 'siteinfo',
+				siprop: 'namespaces'
+			};
+
+			client.api.call(params, callback.apply(this));
+		},
+		'correct arguments are passed to callback': function(e, info /* processed query result */, next, data /* raw data */) {
+			assert.isObject(info);
+			assert.isUndefined(next); // no more pages
+			assert.isObject(data);
+		},
+		'valid processed data is passed to callback': function(e, info /* processed query result */, next, data /* raw data */) {
+			// processed data
+			assert.isObject(info.namespaces);
+			assert.isObject(info.namespaces[0]);
+		},
+		'valid raw data is passed to callback': function(e, info /* processed query result */, next, data /* raw data */) {
+			// raw data
+			assert.isObject(data.query);
+			assert.isObject(data.query.namespaces);
+			assert.isObject(data.query.namespaces[0]);
+		}
+	},
 	'getArticle()': {
 		topic: function() {
 			client.getArticle(ARTICLE, callback.apply(this));
@@ -33,6 +61,33 @@ vows.describe('Mediawiki API').addBatch({
 		},
 		'valid content is passed to callback': function(e, res) {
 			assert.isTrue(res.indexOf("'''Albert Einstein'''") !== false);
+		}
+	},
+	'getImagesFromArticle()': {
+		topic: function() {
+			client.getImagesFromArticle(ARTICLE, callback.apply(this));
+		},
+		'array is passed to callback': function(e, res) {
+			assert.isArray(res);
+		},
+		'valid list of images is passed to callback': function(e, res) {
+			var firstItem = res[0];
+
+			assert.isTrue(firstItem.ns === 6);
+			assert.isTrue(firstItem.title.indexOf("File:") === 0);
+		}
+	},
+	'getExternalLinks()': {
+		topic: function() {
+			client.getExternalLinks(ARTICLE, callback.apply(this));
+		},
+		'array is passed to callback': function(e, res) {
+			assert.isArray(res);
+		},
+		'valid list of external links is passed to callback': function(e, res) {
+			var firstItem = res[0];
+
+			assert.isString(firstItem['*']);
 		}
 	}
 }).export(module);
