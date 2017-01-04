c2e6e916ff0745d34010dee9281b32b30a1602c5
macbre <maciej.brencz@gmail.com>
2017-01-04 23:31:29 +0100

    specialLog2csv.js: generate CSV entries after each batch - save precious memory bytes :)

--- examples/specialLog2csv.js
+++ examples/specialLog2csv.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 /**
- * Example script generating CSV file from Special:Log entries
+ * Example script generating TSV file from Special:Log entries
  *
  * @see https://pl.wikipedia.org/w/api.php?action=help&modules=query%2Blogevents
  */
 const async = require('async'),
 	bot = require('..'),
+	csv = require('csv-string'),
 	client = new bot({
 		server: 'pl.wikipedia.org',
 		path: '/w',
 		debug: true
 	}),
-	logType = 'thanks';
-	//logType = 'review/approve';
+	//logType = 'thanks';
+	logType = 'review/approve';
+
+// @see https://www.npmjs.com/package/csv-string#stringifyinput--object-separator--string--string
+function writeCsvLine(data) {
+	process.stdout.write(csv.stringify(data, "\t"));
+}
 
 // @see https://github.com/caolan/async#whilsttest-fn-callback
-var start = '',
-	logEntries = [];
+var start = '';
 
 async.whilst(
 	() => true, // run as long as there's more data
@@ -24,7 +29,19 @@ async.whilst(
 		console.error('Getting %s logs since %s...', logType, start);
 
 		client.getLog(logType, start, function(err, data, next) {
-			logEntries = logEntries.concat(data);
+			const len = data && data.length;
+
+			client.log('Got %s log entries', len);
+
+			if (len > 0) {
+				// it's our first batch of data - format CSV header
+				if (start === '') {
+					writeCsvLine(Object.keys(data[0]));
+				}
+
+				// print rows
+				data.forEach(writeCsvLine);
+			}
 
 			// next time get next batch
 			start = next;
@@ -32,23 +49,8 @@ async.whilst(
 		});
 	},
 	function(err) {
-		const csv = require('csv-string'),
-			len = logEntries.length;
+		if (err) throw err;
 
-		function writeCsvLine(data) {
-			process.stdout.write(csv.stringify(data));
-		}
-
-		console.error('Got %s log entries', len);
-
-		if (len === 0) {
-			return;
-		}
-
-		// format CSV header
-		writeCsvLine(Object.keys(logEntries[0]));
-
-		// and next rows...
-		logEntries.forEach(writeCsvLine);
+		client.log('Done');
 	}
 );
