6bfb13e04d0efde1ff8e751fce9a454846b3dc95
lmn8 <60514150+lmn8@users.noreply.github.com>
2020-06-11 20:16:07 +0100

    Now eslint compliant

--- examples/scribuntoConsole.js
+++ examples/scribuntoConsole.js
@@ -1,38 +1,38 @@
 var bot = require( 'nodemw' ),
-    readline = require( 'readline' ),
-    rl = readline.createInterface( {
-        input: process.stdin,
-        output: process.stdout
-    } ),
-    client = new bot( {
-        protocol: 'https',
-        server: 'dev.fandom.com',
-        path: ''
-    } ),
-    params = {
-        action: 'scribunto-console',
-        title: 'Module:CLI/testcases/title',
-        clear: true
-    };
+	readline = require( 'readline' ),
+	rl = readline.createInterface( {
+		input: process.stdin,
+		output: process.stdout
+	} ),
+	client = new Bot( {
+		protocol: 'https',
+		server: 'dev.fandom.com',
+		path: ''
+	} ),
+	params = {
+		action: 'scribunto-console',
+		title: 'Module:CLI/testcases/title',
+		clear: true
+	};
 
 function session( err, content ) {
-    params.content = content;
-    rl.on( 'line', cli );
+	params.content = content;
+	rl.on( 'line', cli );
 }
 
 function cli( input ) {
-    params.question = input;
-    client.api.call( params, call );
+	params.question = input;
+	client.api.call( params, call );
 }
 
 function call( err, info, next, data ) {
-    if ( err ) {
-        console.error( err );
-    } else if ( data.type === 'error' ) {
-        console.error( data.message );
-    } else {
-        console.log( data.print );
-    }
+	if ( err ) {
+		console.error( err );
+	} else if ( data.type === 'error' ) {
+		console.error( data.message );
+	} else {
+		console.log( data.print );
+	}
 }
 
 client.getArticle( params.title, session );
--- examples/scribuntoRemoteDebug.js
+++ examples/scribuntoRemoteDebug.js
@@ -1,39 +1,39 @@
 var bot = require( 'nodemw' ),
-    readline = require( 'readline' ),
-    fs = require( 'fs' ),
-    rl = readline.createInterface( {
-        input: process.stdin,
-        output: process.stdout
-    } ),
-    client = new bot( {
-        protocol: 'https',
-        server: 'dev.fandom.com',
-        path: ''
-    } ),
-    params = {
-        action: 'scribunto-console',
-        title: 'Module:CLI/testcases/title',
-        clear: true
-    };
+	readline = require( 'readline' ),
+	fs = require( 'fs' ),
+	rl = readline.createInterface( {
+		input: process.stdin,
+		output: process.stdout
+	} ),
+	client = new Bot( {
+		protocol: 'https',
+		server: 'dev.fandom.com',
+		path: ''
+	} ),
+	params = {
+		action: 'scribunto-console',
+		title: 'Module:CLI/testcases/title',
+		clear: true
+	};
 
-function session( err, data ) {
-    params.content = data;
-    rl.on( 'line', cli );
+function call( err, info, next, data ) {
+	if ( err ) {
+		console.error( err );
+	} else if ( data.type === 'error' ) {
+		console.error( data.message );
+	} else {
+		console.log( data.print );
+	}
 }
 
 function cli( input ) {
-    params.question = input;
-    client.api.call( params, call );
+	params.question = input;
+	client.api.call( params, call );
 }
 
-function call( err, info, next, data ) {
-    if ( err ) {
-        console.error( err );
-    } else if ( data.type === 'error' ) {
-        console.error( data.message );
-    } else {
-        console.log( data.print );
-    }
+function session( err, data ) {
+	params.content = data;
+	rl.on( 'line', cli );
 }
 
 fs.readFile( 'title.lua', session );
