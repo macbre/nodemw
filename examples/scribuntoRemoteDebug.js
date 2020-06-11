51b3e561429de6b8c47995cc122ff9116b84a236
lmn8 <60514150+lmn8@users.noreply.github.com>
2020-06-11 20:00:23 +0100

    Add Scribunto console/remoting examples

---
+++ examples/scribuntoRemoteDebug.js
@@ -0,0 +1,39 @@
+var bot = require( 'nodemw' ),
+    readline = require( 'readline' ),
+    fs = require( 'fs' ),
+    rl = readline.createInterface( {
+        input: process.stdin,
+        output: process.stdout
+    } ),
+    client = new bot( {
+        protocol: 'https',
+        server: 'dev.fandom.com',
+        path: ''
+    } ),
+    params = {
+        action: 'scribunto-console',
+        title: 'Module:CLI/testcases/title',
+        clear: true
+    };
+
+function session( err, data ) {
+    params.content = data;
+    rl.on( 'line', cli );
+}
+
+function cli( input ) {
+    params.question = input;
+    client.api.call( params, call );
+}
+
+function call( err, info, next, data ) {
+    if ( err ) {
+        console.error( err );
+    } else if ( data.type === 'error' ) {
+        console.error( data.message );
+    } else {
+        console.log( data.print );
+    }
+}
+
+fs.readFile( 'title.lua', session );
