7bbbf2d0cd598f682e2bc2d0ae41e39a54e06ab9
macbre <maciej.brencz@gmail.com>
2022-07-01 12:50:56 +0100

    Initial jest setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+/** @type {import('jest').Config} */
+module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  testRegex: "(test).*-test.js$",
+  reporters: ["default", "github-actions" /* https://jestjs.io/blog/2022/04/25/jest-28#github-actions-reporter */],
+  coverageThreshold: {
+    lines: 80
+  }
+};
