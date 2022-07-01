f583a55d88362a1b64269993c7b866b01b0ebcd9
macbre <maciej.brencz@gmail.com>
2022-07-01 14:37:37 +0100

    jest: collect code coverage

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,9 @@ module.exports = {
 	testEnvironment: 'node',
 	testRegex: '(test).*-test.js$',
 	reporters: [ 'default', 'github-actions' /* https://jestjs.io/blog/2022/04/25/jest-28#github-actions-reporter */],
+	collectCoverage: true,
 	coverageThreshold: {
-		lines: 80
+		lines: 33
 	},
 	testTimeout: 2000 // one sec
 };
