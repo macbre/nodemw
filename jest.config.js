4ad5d45dff8a239e2b6d767d59b7c95da84b7334
macbre <maciej.brencz@gmail.com>
2022-07-01 12:59:39 +0100

    test/utils-test.js ported to jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
+'use strict';
 // @ts-check
 /** @type {import('jest').Config} */
 module.exports = {
-  verbose: true,
-  testEnvironment: 'node',
-  testRegex: "(test).*-test.js$",
-  reporters: ["default", "github-actions" /* https://jestjs.io/blog/2022/04/25/jest-28#github-actions-reporter */],
-  coverageThreshold: {
-    lines: 80
-  }
+	verbose: true,
+	testEnvironment: 'node',
+	testRegex: '(test).*-test.js$',
+	reporters: [ 'default', 'github-actions' /* https://jestjs.io/blog/2022/04/25/jest-28#github-actions-reporter */],
+	coverageThreshold: {
+		lines: 80
+	}
 };
