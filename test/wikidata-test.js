e520f40645075a069d2707a334c2346efda9a196
macbre <maciej.brencz@gmail.com>
2023-01-25 17:13:17 +0000

    Code reformat

--- test/wikidata-test.js
+++ test/wikidata-test.js
@@ -1,28 +1,34 @@
 // @ts-check
+"use strict";
+
 const { describe, it, expect } = require("@jest/globals");
 const WikiData = require("../lib/wikidata");
 
 describe("WikiData API", () => {
-    const TEST_ARTICLE = "Albert Einstein";
-    const NOT_EXISTING_ARTICLE = "FooBar39786123";
-    const client = new WikiData();
+  const TEST_ARTICLE = "Albert Einstein";
+  const NOT_EXISTING_ARTICLE = "FooBar39786123";
+  const client = new WikiData();
 
-    describe("getArticleSitelinks()", () => {
-        it(`returns sitelinks for "${TEST_ARTICLE}" article`, async() => {
-            const res = await client.getArticleSitelinks(TEST_ARTICLE);
-            expect(res).toMatchObject({
-                enwiki: {
-                    site: 'enwiki', title: 'Albert Einstein', badges: [ 'Q17437798' ]
-                },
-                plwiki: {
-                    site: 'plwiki', title: 'Albert Einstein', badges: []
-                }
-            });
-        }, 5000);
+  describe("getArticleSitelinks()", () => {
+    it(`returns sitelinks for "${TEST_ARTICLE}" article`, async () => {
+      const res = await client.getArticleSitelinks(TEST_ARTICLE);
+      expect(res).toMatchObject({
+        enwiki: {
+          site: "enwiki",
+          title: "Albert Einstein",
+          badges: ["Q17437798"],
+        },
+        plwiki: {
+          site: "plwiki",
+          title: "Albert Einstein",
+          badges: [],
+        },
+      });
+    }, 5000);
 
-        it(`gives null for not existing article`, async() => {
-            const res = await client.getArticleSitelinks(NOT_EXISTING_ARTICLE);
-            expect(res).toBeNull();
-        }, 5000);
-    });
+    it(`gives null for not existing article`, async () => {
+      const res = await client.getArticleSitelinks(NOT_EXISTING_ARTICLE);
+      expect(res).toBeNull();
+    }, 5000);
+  });
 });
